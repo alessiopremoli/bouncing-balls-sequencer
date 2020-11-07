4095d0089bf72b306e0a7e140e496f60f7a54c4c
“Alessio <alessio.premoli@gmail.com>
2020-11-07 18:51:15 +0100

    a lot

--- js/bouncingBalls.js
+++ js/bouncingBalls.js
@@ -1,4 +1,3 @@
-radius = 4;
 speedX = 2;
 speedY = 3;
 circles = [];
@@ -6,6 +5,9 @@ splittedWidth = [];
 splittedHeight = [];
 notes = (new Array(12)).fill(1).map((v, i) => v = +i + 1);
 octave = '3';
+timePressed = 0;
+MIN_RADIUS = 4;
+MAX_RADIUS = 100;
 
 function setup() {
     createCanvas(1200, 600);
@@ -25,27 +27,27 @@ function setup() {
 }
 
 function draw() {
-    background(255, 0   );
+    background(255, 0);
     clear();
     for (i = 0; i < circles.length; i++) {
         // console.warn(circles[i]);
-        circles[i] = drawBall(circles[i].x, circles[i].y, circles[i].speedX, circles[i].speedY);
+        circles[i] = drawBall(circles[i].x, circles[i].y, circles[i].speedX, circles[i].speedY, circles[i].radius);
     }
 }
 
-function drawBall(xCo, yCo, speedXCo, speedYCo) {
+function drawBall(xCo, yCo, speedXCo, speedYCo, radiusCo) {
     // Add the current speed to the x location.
     xCo = xCo + speedXCo;
     yCo = yCo + speedYCo
 
     let selectedNote;
-    if ((xCo > width - radius) || (xCo < radius)) {
+    if ((xCo >= width - radiusCo / 2) || (xCo <= radiusCo / 2)) {
         // If the object reaches either edge, multiply speed by -1 to turn it around.
         speedXCo = speedXCo * -1;
         selectedNote = splittedHeight.filter(el => yCo >= el.start && yCo < el.end)[0];
     }
 
-    if ((yCo > height - radius) || (yCo < radius)) {
+    if ((yCo >= height - radiusCo / 2) || (yCo <= radiusCo / 2)) {
         // If the object reaches either edge, multiply speed by -1 to turn it around.
         speedYCo = speedYCo * -1;
         selectedNote = splittedWidth.filter(el => xCo >= el.start && xCo < el.end)[0];
@@ -53,37 +55,42 @@ function drawBall(xCo, yCo, speedXCo, speedYCo) {
     }
 
     if (selectedNote) {
-        console.log();
-        // let hw = width / 2;
-        // pan = new Tone.Panner((xCo - hw) / hw);
-        // console.log((yCo - hw) / hw);
-        // synth = new Tone.Synth();
-        // synth.connect(pan);
-        // pan.toDestination();
-
         let hw = width / 2;
-        pan = (xCo - hw) / hw
+        pan = (xCo - hw) / hw;
 
-        let synth = winterSynth(pan);
+        synth = winterSynth(pan, map(radiusCo, MIN_RADIUS, MAX_RADIUS, -6, 0));
         synth.triggerAttackRelease(`${mapNumberToNoteWinterAirport(selectedNote.note, octave)}`, "4n");
-        synth = null;
     }
 
-    stroke(0);
-    fill(175);
-    ellipse(xCo, yCo, radius, radius);
-    return { x: xCo, y: yCo, speedX: speedXCo, speedY: speedYCo }
+    stroke(0, 120);
+    fill(175, 120);
+    ellipse(xCo, yCo, radiusCo, radiusCo);
+    return { x: xCo, y: yCo, speedX: speedXCo, speedY: speedYCo, radius: radiusCo }
 }
 
 function mousePressed() {
     if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+        timePressed = millis();
+    }
+}
+
+function mouseReleased() {
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+        timePressed = millis() - timePressed;
         let xRand = random([-1, 1]);
         let yRand = random([-1, 1]);
-        circles.push({ x: mouseX, y: mouseY, speedX: xRand * speedX, speedY: yRand * speedY });
+        circles.push({
+            x: mouseX,
+            y: mouseY,
+            speedX: xRand * speedX,
+            speedY: yRand * speedY,
+            radius: Math.min(map(min(timePressed, 4000), 0, 4000, MIN_RADIUS, MAX_RADIUS), width - mouseX - 1, height - mouseY - 1)
+        });
     }
     return false;
 }
 
 function resetBalls() {
     circles = [];
+    synthArray = [];
 }
--- js/synthsFactory.js
+++ js/synthsFactory.js
@@ -2,18 +2,34 @@ const reverb = new Tone.Reverb({
     decay: 15,
     wet: 0.5
 });
+let tVolume = null;
+let tFilter = null;
+let tPan = null;
 
-const winterSynth = panning => {
+synthArray = [];
 
-    pan = new Tone.Panner(panning);
-    synth = new Tone.Synth();
+const winterSynth = (panning, vol) => {
+    console.log(vol);
+    tVolume = new Tone.Volume(vol);
+    tPan = new Tone.Panner(panning);
+    
+    synth = new Tone.Synth({
+        envelope: { release: 2 }
+    });
     tFilter = new Tone.Filter(800, "lowpass");
 
-    synth.connect(tFilter);
-    tFilter.connect(pan);
-    pan.connect(reverb);
+    synth.connect(tVolume);
+    tVolume.connect(tFilter);
+    tFilter.connect(tPan);
+    tPan.connect(reverb);
     reverb.toDestination();
 
-
     return synth;
+}
+
+const disposeSynth = () => {
+    tPan && tPan.dispose();
+    tFilter && tFilter.dispose();
+    tVolume && tVolume.dispose();
+    synth && synth.dispose();
 }
